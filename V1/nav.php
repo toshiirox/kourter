a5fcf000a5b03eaa94f5671722ed5df623fa4f84
Corentin Chateau <Corentin.Chateau.49@gmail.com>
2016-08-29 15:35:23 +0200

    Changements Basiques

---
+++ V1/nav.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="../V1/home.php">Kourter</a>
+    </div>
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">- -</a></li>
+        <li><a href="#">- -</a></li>
+        <li><a href="#">- -</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+            <span class="glyphicon glyphicon-user"></span>&nbsp;Bonjour <?php echo $userRow['user_email']; ?>&nbsp;<span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span>&nbsp;Voir Profil</a></li>
+              <li><a href="logout.php?logout=true"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Deconnexion</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </nav>
