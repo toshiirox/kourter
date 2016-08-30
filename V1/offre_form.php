a99a288ab0a009207a0e6d9e18453e12632ff4f2
Hugovics <hcournee@gmail.com>
2016-08-30 15:22:08 +0200

    Form

---
+++ V1/offre_form.php
@@ -0,0 +1,26 @@
+<div class="container">
+	<form class="offre_form">
+
+		<form method="post">
+		<h1>Nouvelle Offre</h1>
+			<input type="text" name="Banque" placeholder="ex: Société Générale" required="required" />
+			<input type="text" name="Agence" placeholder="Nom de L'Agence" required="required" />
+			<input type="text" name="Adresse" placeholder="Adresse" required="required" />
+			<input type="text" name="Code Postal" placeholder="" required="required" />
+			<label for=codepostal>Code postal</label>
+            <input id=codepostal name=codepostal type=text required>
+
+
+
+
+
+
+
+
+
+			<button type="submit" class="btn btn-primary btn-block btn-large">Let me in.</button>
+		</form>
+	</div>
+</div>
+</div>
+</div>
