d8ac971a60e41a472ec1ddb1b29af6d1f4fc820c
François maitre jedi <francois.m2102@gmail.com>
2016-08-30 14:06:50 +0200

    BDD MODIF

--- taux_credit.sql
+++ taux_credit.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 30 Août 2016 à 09:55
+-- Généré le :  Mar 30 Août 2016 à 14:06
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -23,18 +23,71 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `banque`
+-- Structure de la table `agence`
 --
 
-CREATE TABLE IF NOT EXISTS `banque` (
+CREATE TABLE IF NOT EXISTS `agence` (
   `bank_id` int(11) NOT NULL AUTO_INCREMENT,
-  `bank_name` varchar(25) DEFAULT NULL,
-  `bank_logo` varchar(60) DEFAULT NULL,
   `bank_adress` varchar(60) DEFAULT NULL,
   `bank_city` varchar(25) DEFAULT NULL,
   `bank_cp` int(11) DEFAULT NULL,
-  PRIMARY KEY (`bank_id`)
+  `id_bank` int(11) DEFAULT NULL,
+  PRIMARY KEY (`bank_id`),
+  KEY `FK_agence_id_bank` (`id_bank`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `caracteristiques`
+--
+
+CREATE TABLE IF NOT EXISTS `caracteristiques` (
+  `id_caracteristique` int(11) NOT NULL AUTO_INCREMENT,
+  `budget` int(11) DEFAULT NULL,
+  `duree` int(11) DEFAULT NULL,
+  `type_project` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`id_caracteristique`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `defini`
+--
+
+CREATE TABLE IF NOT EXISTS `defini` (
+  `id_caracteristique` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  PRIMARY KEY (`id_caracteristique`,`project_id`),
+  KEY `FK_defini_project_id` (`project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `enseigne`
+--
+
+CREATE TABLE IF NOT EXISTS `enseigne` (
+  `id_bank` int(11) NOT NULL AUTO_INCREMENT,
+  `bank_logo` varchar(60) DEFAULT NULL,
+  `bank_name` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`id_bank`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `gere`
+--
+
+CREATE TABLE IF NOT EXISTS `gere` (
+  `id_offre` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id_offre`,`user_id`),
+  KEY `FK_gere_user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -43,15 +96,10 @@ CREATE TABLE IF NOT EXISTS `banque` (
 --
 
 CREATE TABLE IF NOT EXISTS `offre` (
-  `offre_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_offre` int(11) NOT NULL AUTO_INCREMENT,
   `taux` float DEFAULT NULL,
-  `duree_mini` int(11) DEFAULT NULL,
-  `montant_mini` int(11) DEFAULT NULL,
-  `type_project` varchar(25) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
   `bank_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`offre_id`),
-  KEY `FK_offre_user_id` (`user_id`),
+  PRIMARY KEY (`id_offre`),
   KEY `FK_offre_bank_id` (`bank_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -63,7 +111,6 @@ CREATE TABLE IF NOT EXISTS `offre` (
 
 CREATE TABLE IF NOT EXISTS `project` (
   `project_id` int(11) NOT NULL AUTO_INCREMENT,
-  `budget` int(11) DEFAULT NULL,
   `apport_perso` int(11) DEFAULT NULL,
   `nom_client` varchar(25) DEFAULT NULL,
   `prenom_client` varchar(25) DEFAULT NULL,
@@ -72,11 +119,23 @@ CREATE TABLE IF NOT EXISTS `project` (
   `revenu_client` int(11) DEFAULT NULL,
   `taux_endettement` float DEFAULT NULL,
   `situation` varchar(25) DEFAULT NULL,
-  `type_project` varchar(25) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`project_id`),
   KEY `FK_project_user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `recquiert`
+--
+
+CREATE TABLE IF NOT EXISTS `recquiert` (
+  `id_offre` int(11) NOT NULL,
+  `id_caracteristique` int(11) NOT NULL,
+  PRIMARY KEY (`id_offre`,`id_caracteristique`),
+  KEY `FK_recquiert_id_caracteristique` (`id_caracteristique`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -90,31 +149,50 @@ CREATE TABLE IF NOT EXISTS `users` (
   `user_name` varchar(25) DEFAULT NULL,
   `user_pass` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
---
--- Contenu de la table `users`
---
-
-INSERT INTO `users` (`user_id`, `user_email`, `user_name`, `user_pass`) VALUES
-(1, 'francois.m@gmail.com', 'francoism', '$2y$10$WHCKF7Oyo1WQD26js46cJumjKJpSHRGcM3w1w1m463BLZj6746ZBq');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- Contraintes pour les tables exportées
 --
 
 --
+-- Contraintes pour la table `agence`
+--
+ALTER TABLE `agence`
+  ADD CONSTRAINT `FK_agence_id_bank` FOREIGN KEY (`id_bank`) REFERENCES `enseigne` (`id_bank`);
+
+--
+-- Contraintes pour la table `defini`
+--
+ALTER TABLE `defini`
+  ADD CONSTRAINT `FK_defini_project_id` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`),
+  ADD CONSTRAINT `FK_defini_id_caracteristique` FOREIGN KEY (`id_caracteristique`) REFERENCES `caracteristiques` (`id_caracteristique`);
+
+--
+-- Contraintes pour la table `gere`
+--
+ALTER TABLE `gere`
+  ADD CONSTRAINT `FK_gere_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `FK_gere_id_offre` FOREIGN KEY (`id_offre`) REFERENCES `offre` (`id_offre`);
+
+--
 -- Contraintes pour la table `offre`
 --
 ALTER TABLE `offre`
-  ADD CONSTRAINT `FK_offre_bank_id` FOREIGN KEY (`bank_id`) REFERENCES `banque` (`bank_id`),
-  ADD CONSTRAINT `FK_offre_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+  ADD CONSTRAINT `FK_offre_bank_id` FOREIGN KEY (`bank_id`) REFERENCES `agence` (`bank_id`);
 
 --
 -- Contraintes pour la table `project`
 --
 ALTER TABLE `project`
   ADD CONSTRAINT `FK_project_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Contraintes pour la table `recquiert`
+--
+ALTER TABLE `recquiert`
+  ADD CONSTRAINT `FK_recquiert_id_caracteristique` FOREIGN KEY (`id_caracteristique`) REFERENCES `caracteristiques` (`id_caracteristique`),
+  ADD CONSTRAINT `FK_recquiert_id_offre` FOREIGN KEY (`id_offre`) REFERENCES `offre` (`id_offre`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
