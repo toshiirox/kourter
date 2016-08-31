ad81cc9867b21b7efdc7a0f794e42dbf85b23132
Corentin Chateau <Corentin.Chateau.49@gmail.com>
2016-08-31 14:26:29 +0200

    Commit.

---
+++ V1/class.projet.php
@@ -0,0 +1,86 @@
+<?php 
+
+require_once("dbconfig.php");
+
+
+class projet {
+	public $id_projet;
+	public $budget_projet;
+	public $nom_client;
+	public $prenom_client;
+	public $situation_client;
+	public $revenu_client;
+	public $duree_emprunt;
+	public $type_projet;
+	public $user_id;
+	
+	
+	public function __construct($id_projet,$budget_projet,$nom_client,$prenom_client,$situation_client,$revenu_client,$duree_emprunt,$type_projet,$user_id){
+		$this->conn = $db;
+		$stmt=$connexion->prepare("INSERT INTO kourter.projet (id_projet,budget_projet,nom_client,prenom_client,situation_client,revenu_client,duree_emprunt,type_projet,user_id) VALUES (NULL, :id_projet, :budget_projet, :nom_client, :prenom_client, :situation_client, :revenu_client, :duree_emprunt, :type_projet, :user_id)");
+		$stmt->bindValue(":id_projet", $id_projet);
+		$stmt->bindValue(":budget_projet", $budget_projet);
+		$stmt->bindValue(":nom_client", $nom_client);
+		$stmt->bindValue(":prenom_client", $prenom_client);
+		$stmt->bindValue(":situation_client", $situation_client);
+		$stmt->bindValue(":revenu_client", $revenu_client);
+		$stmt->bindValue(":duree_emprunt", $duree_emprunt);
+		$stmt->bindValue(":type_projet", $type_projet);
+		$stmt->bindValue(":user_id", $user_id);
+
+
+
+		$stmt->execute();
+		$bank=$stmt->fetchObject();
+		$this->id_projet=$id_projet;
+		$this->budget_projet=$budget_projet;
+		$this->nom_client=$nom_client;
+		$this->prenom_client=$prenom_client;
+		$this->situation_client=$situation_client;
+		$this->revenu_client=$revenu_client;
+		$this->duree_emprunt=$duree_emprunt;
+		$this->type_projet=$type_projet;
+		$this->user_id=$user_id;
+
+	}
+
+	public static function getAllfromProjet(){
+		$this->conn =$db;
+		$req=$this->conn->query("SELECT * FROM projet;");
+		$req->setFetchMode(PDO::FETCH_OBJ);
+		return $req;
+	}
+
+	public static function getProjetbyType($type){
+		$this->conn =$db;
+		$req=$this->conn->query("SELECT * FROM projet WHERE type_projet=:type;");
+		$req->bindValue(':type',$type);
+		$req->setFetchMode(PDO::FETCH_OBJ);
+		return $req;
+	}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
--- V1/header.php
+++ V1/header.php
@@ -1,3 +1,19 @@
+<?php
+
+require_once("session.php");
+
+require_once("class.user.php");
+$auth_user = new USER();
+
+
+$user_id = $_SESSION['user_session'];
+
+$stmt = $auth_user->runQuery("SELECT * FROM users WHERE user_id=:user_id");
+$stmt->execute(array(":user_id"=>$user_id));
+
+$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
