5bded89d73370b1fec51bc9e4fdf0996c62a7f45
François Mourier <francois.m2102@gmail.com>
2016-08-31 14:26:53 +0200

    classe offre

---
+++ V1/class.offre.php
@@ -0,0 +1,64 @@
+<?php 
+require_once("dbconfig.php");
+
+class offre_banque {
+	public $id_offre;
+	public $nom_agence;
+	public $taux_offre;
+	public $montant_mini;
+	public $type_offre;
+	public $user_id;	
+
+	public function __construct($id_offre,$nom_agence,$taux_offre,$montant_mini,$type_offre,$user_id){
+		$this->conn = $db;
+		$stmt=$connexion->prepare("INSERT INTO kourter.offre_banque (id_offre, nom_agence, taux_offre, montant_mini, type_offre, user_id) VALUES (NULL, :nom_agence, :taux_offre, :montant_mini, :type_offre, :user_id)");
+		$stmt->bindValue(':nom_agence',$nom_agence);
+		$stmt->bindValue(':taux_offre',$taux_offre);
+		$stmt->bindValue(':montant_mini',$montant_mini);
+		$stmt->bindValue(':type_offre',$type_offre);
+		$stmt->bindValue(':user_id',$user_id);
+		$stmt->execute();
+		$this->nom_agence=$nom_agence;
+		$this->taux_offre=$taux_offre;
+		$this->montant_mini=$montant_mini;
+		$this->type_offre=$type_offre;
+		$this->user_id=$user_id;
+	}
+
+	public static function getAllfromBanks(){
+		$this->conn = $db;
+		$req=$this->conn->query("SELECT * FROM offre_banque");
+		$req->setFetchMode(PDO::FETCH_OBJ);
+		return $req;
+	}
+
+	public static function getOffreByType($type){
+		$this->conn = $db;
+		$req=$this->conn->query("SELECT * FROM offre_banque WHERE type_offre=:type")
+		$req->bindValue(':type',$type);
+		$req->setFetchMode(PDO::FETCH_OBJ);
+		return $req;
+	}
+
+	/*public static function getTauxById($id_offre){
+		global $connexion;
+		$req=$connexion->prepare("SELECT taux_offre From offre_banque WHERE ID=:id");
+		$req->bindValue(":id",$id_offre);
+		$Taux1=$req->fetchObject();
+		return $Taux1;
+
+	}*/
+
+	public function Text(){
+		echo $this->Nom." ".$this->Taux;
+	}
+}
+
+/*$bank1=new Banque('1');*/
+/*$bank1->Text();*/
+/*$listeBank=Banque::getAllBanks();
+while ($ligne=$listeBank->fetch()) {
+			echo $ligne->ID." ".$ligne->Nom." ".$ligne->Taux." ".$ligne->Date_modif." <br>";
+		}*/
+
+		?>
