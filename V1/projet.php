ec507655b0062394b7047fb4a1d18b308f3770db
Corentin Chateau <Corentin.Chateau.49@gmail.com>
2016-09-01 17:02:49 +0200

    Commot ùal,fdùqksnsdf

--- V1/projet.php
+++ V1/projet.php
@@ -4,7 +4,7 @@
     <legend><h1>Ajout de Projet</h1></legend>
 
     <div class="row marge-20">
-    <div class= "title">
+      <div class= "title">
        <div class="col-md-4 col-md-offset-4">Informations Client</div>
      </div>
 
@@ -59,65 +59,63 @@
         </div>
       </div>
 
-</fieldset>
-</form>
+    </fieldset>
 
 
 
-<div class="row marge-20">
-    <div class= "title">
+    <div class="row marge-20">
+      <div class= "title">
        <div class="col-md-4 col-md-offset-4">Informations Projet</div>
      </div>
 
    </div>
 
-<form class="form-horizontal" method="POST" action="projet_ok.php">
-    <fieldset>
+   <fieldset>
 
-      <!-- Form Name --> 
-
+    <!-- Form Name --> 
 
 
-     <!-- Text input-->
-      <div class="form-group">
-        <label class="col-md-4 control-label" for="budget_projet">Budget</label>  
-        <div class="col-md-4">
-          <input id="budget" name="budget_projet" type="text" placeholder="Ex : 150 000" class="form-control input-md" required="">
 
-        </div>
+    <!-- Text input-->
+    <div class="form-group">
+      <label class="col-md-4 control-label" for="budget_projet">Budget</label>  
+      <div class="col-md-4">
+        <input id="budget" name="budget_projet" type="text" placeholder="Ex : 150 000" class="form-control input-md" required="">
+
       </div>
+    </div>
 
-      <!-- Text input-->
-      <div class="form-group">
-        <label class="col-md-4 control-label" for="duree_emprunt">Durée d'emprunt</label>  
-        <div class="col-md-4">
-          <input id="duree_emprunt" name="duree_emprunt" type="text" placeholder="Ex : 20" class="form-control input-md" required="">
-          <span class="help-block">Durée en année</span>  
-        </div>
+    <!-- Text input-->
+    <div class="form-group">
+      <label class="col-md-4 control-label" for="duree_emprunt">Durée d'emprunt</label>  
+      <div class="col-md-4">
+        <input id="duree_emprunt" name="duree_emprunt" type="text" placeholder="Ex : 20" class="form-control input-md" required="">
+        <span class="help-block">Durée en année</span>  
       </div>
+    </div>
 
-      <!-- Select Basic -->
-      <div class="form-group">
-        <label class="col-md-4 control-label" for="type_offre">Type du projet</label>
-        <div class="col-md-4">
-          <select id="type_offre" name="type_offre" class="form-control">
-            <option value="Immobilier">Immobilier</option>
-            <option value="Automobile">Automobile</option>
-            <option value="Etudiant">Étudiant</option>
-          </select>
-        </div>
+    <!-- Select Basic -->
+    <div class="form-group">
+      <label class="col-md-4 control-label" for="type_offre">Type du projet</label>
+      <div class="col-md-4">
+        <select id="type_offre" name="type_offre" class="form-control">
+          <option value="Immobilier">Immobilier</option>
+          <option value="Automobile">Automobile</option>
+          <option value="Etudiant">Étudiant</option>
+        </select>
       </div>
+    </div>
 
-      <!-- Button -->
-      <div class="form-group">
-        <label class="col-md-4 control-label" for="valid_offre"></label>
-        <div class="col-md-4">
-          <button id="valid_offre" name="valid_offre" class="col-xs-6 col-xs-offset-3 btn btn-valider">Ajouter un projet</button>
-        </div>
+    <!-- Button -->
+    <div class="form-group">
+      <label class="col-md-4 control-label" for="valid_offre"></label>
+      <div class="col-md-4">
+        <button id="valid_offre" name="valid_offre" class="col-xs-6 col-xs-offset-3 btn btn-valider">Ajouter un projet</button>
       </div>
+    </div>
 
-    </fieldset>
-  </form>
+  </fieldset>
+</form>
 
 </div>
 <?php include ('footer.php'); ?>
