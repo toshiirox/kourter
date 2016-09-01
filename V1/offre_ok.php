59f7cbce14f5ee0a6548cc17b8ac139691ad533a
Corentin Chateau <Corentin.Chateau.49@gmail.com>
2016-09-01 19:24:28 +0200

    qsd

--- V1/offre_ok.php
+++ V1/offre_ok.php
@@ -7,11 +7,34 @@ $montant_mini=$_POST["montant_mini"];
 $type_offre=$_POST["type_offre"];
 $user_id=$userRow["user_id"];
 $offre=new offre(NULL,$nom_agence,$taux_offre,$montant_mini,$type_offre,$user_id);
-?>
 
-<body>
-	test on fera du front mamène
-</body>
+
+echo '<div class="container">';
+echo '<div class="panel panel-default">';
+echo '<div class="panel-heading">Agence : '.$nom_agence.'</div>';
+echo '<div class="panel-body">Agence : '.$nom_agence.'</div>';
+echo '<div class="panel-body">Taux  : '.$taux_offre.'</div>';
+echo '<div class="panel-body">Montant Minimum : '.$montant_mini.'</div>';
+echo '<div class="panel-body">Type de l\'offre : '.$type_offre.'</div>';
+
+
+echo '</div>';
+
+
+
+?>
+<form class="form-horizontal">
+	<fieldset>
+		<div class="form-group">
+			<div class="col-md-8">
+				<a class="btn btn-success col-xs-3 col-xs-offset-4 " href="view.offres.php" role="button">Offres</a>
+				<a class="btn btn-info col-xs-3 col-xs-offset-2 " href="offres.php" role="button">Nouvelle Offre</a>
+			</div>
+		</div>
+
+	</fieldset>
+</form>	
+</div>
 
 
 <?php include ('footer.php');  ?>
