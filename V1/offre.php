f66a2ec9bc6c8d633da12c58e06e346c9cbcb60d
Corentin Chateau <Corentin.Chateau.49@gmail.com>
2016-09-01 14:03:58 +0200

    Changement des infos BDD Type

--- V1/offre.php
+++ V1/offre.php
@@ -39,9 +39,9 @@
           <label class="col-md-4 control-label" for="type_offre">Type du projet</label>
           <div class="col-md-4">
             <select id="type_offre" name="type_offre" class="form-control">
-              <option value="immo">Immobilier</option>
-              <option value="auto">Automobile</option>
-              <option value="etudiant">Étudiant</option>
+              <option value="Immobilier">Immobilier</option>
+              <option value="Automobile">Automobile</option>
+              <option value="Etudiant">Etudiant</option>
             </select>
           </div>
         </div>
