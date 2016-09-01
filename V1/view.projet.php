4d20c628c4cc39659db43aada0275467ed51a367
François Mourier <francois.m2102@gmail.com>
2016-09-01 10:21:26 +0200

    vue projet

---
+++ V1/view.projet.php
@@ -0,0 +1,11 @@
+<?php 
+include ('header.php');
+require_once ('class.projet.php');
+
+$listProjet=projet::getAllfromProjet();
+
+while ($ligne=$listProjet->fetch()) {
+	echo $ligne->nom_client;
+}
+
+?>
